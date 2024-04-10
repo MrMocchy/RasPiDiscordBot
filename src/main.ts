2e738b2d1644dce5ace53226225b8020196a5014
MrMocchy <patronummocchy@tiara.ocn.ne.jp>
2024-04-10 21:14:14 +0900

    edit .env and main.ts for OUCC and schedule

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 "use strict"
 
+const schedule = require("node-schedule")
+
 const { ForumStatistics } = require("./ForumStatistics")
 
-new ForumStatistics(
-    process.env.TESTBOT_TOKEN!,
-    [process.env.TESTGUILD_FORUMCHANNELID!],
-    process.env.TESTGUILD_BOTCHANNELID!
-).run()
+// new ForumStatistics(
+//     process.env.TESTBOT_TOKEN!,
+//     [process.env.TESTGUILD_FORUMCHANNELID!],
+//     process.env.TESTGUILD_BOTCHANNELID!
+// ).run()
+
+
+const forumStatisticsJob = schedule.scheduleJob("0 18 * * 6", () => {
+    new ForumStatistics(
+        process.env.OUCC_ECHAN_TOKEN!,
+        [
+            process.env.OUCC_PROJECT_FORUMCHANNELID!,
+            process.env.OUCC_SHARE_FORUMCHANNELID!,
+            process.env.OUCC_OFFTOPIC_FORUMCHANNELID!,
+        ],
+        process.env.OUCC_BOTCHANNELID!
+    ).run()
+})
