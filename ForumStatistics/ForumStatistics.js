d1e84b2df8cb965ada1ad3312205d24b9700592e
MrMocchy <patronummocchy@tiara.ocn.ne.jp>
2025-04-26 21:26:24 +0900

    tsc

--- ForumStatistics/ForumStatistics.js
+++ ForumStatistics/ForumStatistics.js
@@ -198,14 +198,14 @@ class ForumStatistics {
         this.client.login(this.token);
     }
 }
-new ForumStatistics(process.env.TESTBOT_TOKEN, [process.env.TESTGUILD_FORUMCHANNELID], process.env.TESTGUILD_BOTCHANNELID).run(true, true);
 // new ForumStatistics(
-//     process.env.OUCC_ECHAN_TOKEN!,
-//     [
-//         process.env.OUCC_PROJECT_FORUMCHANNELID!,
-//         process.env.OUCC_SHARE_FORUMCHANNELID!,
-//         process.env.OUCC_OFFTOPIC_FORUMCHANNELID!,
-//     ],
-//     process.env.OUCC_BOTCHANNELID!
-// ).run()
+//     process.env.TESTBOT_TOKEN!,
+//     [process.env.TESTGUILD_FORUMCHANNELID!],
+//     process.env.TESTGUILD_BOTCHANNELID!
+// ).run(true, true)
+new ForumStatistics(process.env.OUCC_ECHAN_TOKEN, [
+    process.env.OUCC_PROJECT_FORUMCHANNELID,
+    process.env.OUCC_SHARE_FORUMCHANNELID,
+    process.env.OUCC_OFFTOPIC_FORUMCHANNELID,
+], process.env.OUCC_BOTCHANNELID).run();
 console.log("ForumStatistics is Active!");
